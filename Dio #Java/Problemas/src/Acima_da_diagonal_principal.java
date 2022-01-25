ecfcb246a72c2fc18b41022255a91d4b4e420809
WallaceSilva <50365294+wallaceice@users.noreply.github.com>
2022-01-25 17:15:46 -0300

    Update Acima_da_diagonal_principal.java

--- Dio #Java/Problemas/src/Acima_da_diagonal_principal.java
+++ Dio #Java/Problemas/src/Acima_da_diagonal_principal.java
@@ -24,16 +24,15 @@ public class Acima_da_diagonal_principal {
         double soma = 0;
         double count = 0;
 
-        for (int i = 0; i < 12; ++i) {
-            for (int j = 0; j < 12; ++j) {
+        for (int i = 0; i < 12; ++i)
+            for (int j = 0; j < 12; ++j)
                 array[i][j] = leitor.nextDouble();
-            }
-        }
+        
         for (int j = 1; j < 12; j++)
-            for (int i = 0; i < j; i++) {
+            for (int i = 0; i < j; i++)
                 soma += array[i][j];
                 count++;
-            }
+            
         if (opcao.equals("M")) System.out.printf("%.1f\n", soma / count);
         else System.out.printf("%.1f\n", soma);
     }
